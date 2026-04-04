0e533130cb9623061155f0df1545f5703b901ab6
Alexandre Soares <alexandressoares383@gmail.com>
2026-04-03 21:55:14 -0300

    fix: corrigir testes e lint

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,16 +3,18 @@ import { TextDecoder, TextEncoder } from 'node:util';
 import '@testing-library/jest-dom';
 
 const globalObject = globalThis as typeof globalThis & {
-  TextEncoder: any;
-  TextDecoder: any;
+  TextEncoder?: typeof globalThis.TextEncoder;
+  TextDecoder?: typeof globalThis.TextDecoder;
 };
 
 if (typeof globalThis.TextEncoder === 'undefined') {
-  globalObject.TextEncoder = TextEncoder;
+  globalObject.TextEncoder =
+    TextEncoder as unknown as typeof globalThis.TextEncoder;
 }
 
 if (typeof globalThis.TextDecoder === 'undefined') {
-  globalObject.TextDecoder = TextDecoder;
+  globalObject.TextDecoder =
+    TextDecoder as unknown as typeof globalThis.TextDecoder;
 }
 
 expect.extend({});
